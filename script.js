8f3ea4716cb546413c096a3b75cbaec18a993119
Zafer Yılmaz <83840329+Vircam@users.noreply.github.com>
2022-05-10 14:21:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,81 @@
 let values = [];
-
-let i = 0;
 let w = 10;
 
+let states = [];
+
 function setup() {
-    createCanvas(1080, 600);
-    values = new Array(floor(width/w));
-    for (let i = 0; i < values.length; i++) {
-        values[i] = random(height);
+  createCanvas(windowWidth, windowHeight);
+  values = new Array(floor(width / w));
+  for (let i = 0; i < values.length; i++) {
+    values[i] = random(height);
+    states[i] = -1;
+  }
+  quickSort(values, 0, values.length - 1);
+}
+
+async function quickSort(arr, start, end) {
+  if (start >= end) {
+    return;
+  }
+  let index = await partition(arr, start, end);
+  states[index] = -1;
+
+  await Promise.all([
+    quickSort(arr, start, index - 1),
+    quickSort(arr, index + 1, end)
+  ]);
+}
+
+async function partition(arr, start, end) {
+  for (let i = start; i < end; i++) {
+    states[i] = 1;
+  }
+
+  let pivotValue = arr[end];
+  let pivotIndex = start;
+  states[pivotIndex] = 0;
+  for (let i = start; i < end; i++) {
+    if (arr[i] < pivotValue) {
+      await swap(arr, i, pivotIndex);
+      states[pivotIndex] = -1;
+      pivotIndex++;
+      states[pivotIndex] = 0;
     }
-    frameRate(5);
-    quickSort(values, 0, values.length - 1);
+  }
+  await swap(arr, pivotIndex, end);
+
+  for (let i = start; i < end; i++) {
+    if (i != pivotIndex) {
+      states[i] = -1;
+    }
+  }
+
+  return pivotIndex;
 }
 
 function draw() {
-    background(51);
-    
-    for (let i = 0; i < values.length; i++) {
-        stroke(0);
-        fill(255);
-        rect(i*w, height - values[i], w, values[i]);
-        Sleep(500);
+  background(0);
+
+  for (let i = 0; i < values.length; i++) {
+    noStroke();
+    if (states[i] == 0) {
+      fill('#E0777D');
+    } else if (states[i] == 1) {
+      fill('#D6FFB7');
+    } else {
+      fill(255);
     }
+    rect(i * w, height - values[i], w, values[i]);
+  }
 }
 
-function quickSort(arr, start, end) {
-    if ( start >= end){
-        return;
-    }
-
-    var index = partition(arr, start, end);
-    quickSort(arr, start, index - 1);
-    quickSort(arr, index + 1, end);
-
+async function swap(arr, a, b) {
+  await sleep(50);
+  let temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
 }
 
-function partition(arr, start, end){
-    let pivotIndex = start;
-    let pivotValue = arr[end];
-    for(let i = start; i < end; i++){
-        if ( arr[i] < pivotValue){
-            swap(arr, i, pivotIndex);
-            pivotIndex++;
-        }
-    }
-
-    swap(arr, pivotIndex, end);
-    return pivotIndex;
-}
-
-function swap(arr, a, b) {
-    let temp = arr[a];
-    arr[a] = arr[b];
-    arr[b] = temp;
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
